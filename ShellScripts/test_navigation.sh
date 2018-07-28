6289229ba4331c7f64e6dd9f0c53dd60b97d1be8
Hamza CHETTOUR <hamzachettour@gmail.com>
2018-07-28 23:10:31 +0000

    feat: use amcl from turtle_gazebo instead of turtle_navigation

---
+++ ShellScripts/test_navigation.sh
@@ -0,0 +1,5 @@
+#!/bin/sh 
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$UGLY_W_WORLD
+" & sleep 10
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/ros_catkin_ws/src/Simple-home-service-robot/Worlds/Ugly_W_map.yaml" & sleep 5
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" 
